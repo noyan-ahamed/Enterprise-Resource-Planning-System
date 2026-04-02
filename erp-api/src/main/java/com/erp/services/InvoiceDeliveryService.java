534f1f22ec5a9a3a69584957457cc29d6142612f
noyan-ahamed <noyanahamed7000@gmail.com>
2026-04-02 18:55:38 +0600

    Update admin layout, remove unused sub menu, add email service API

--- erp-api/src/main/java/com/erp/services/InvoiceDeliveryService.java
+++ erp-api/src/main/java/com/erp/services/InvoiceDeliveryService.java
@@ -6,7 +6,7 @@ import com.erp.enities.SalesOrderHeader;
 public interface InvoiceDeliveryService {
     void generateInvoice(SalesOrderHeader salesOrder);
     void printInvoice(SalesOrderHeader salesOrder);
-    void emailInvoice(SalesOrderHeader salesOrder);
+    void emailInvoice(String to, String subject, String body);
 
     void notifySellerPaymentApproved(CustomerPayment payment);
 }
--- erp-api/src/main/java/com/erp/services/implemented/InvoiceDeliveryServiceImplement.java
+++ erp-api/src/main/java/com/erp/services/implemented/InvoiceDeliveryServiceImplement.java
@@ -3,13 +3,18 @@ package com.erp.services.implemented;
 import com.erp.enities.CustomerPayment;
 import com.erp.enities.SalesOrderHeader;
 import com.erp.services.InvoiceDeliveryService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class InvoiceDeliveryServiceImplement implements InvoiceDeliveryService {
 
+    private final JavaMailSender javaMailSender;
     @Override
     public void generateInvoice(SalesOrderHeader salesOrder) {
         log.info("Invoice generation placeholder for sale invoice: {}", salesOrder.getInvoiceNumber());
@@ -20,9 +25,22 @@ public class InvoiceDeliveryServiceImplement implements InvoiceDeliveryService {
         log.info("Invoice print placeholder for sale invoice: {}", salesOrder.getInvoiceNumber());
     }
 
+//    @Override
+//    public void emailInvoice(SalesOrderHeader salesOrder) {
+//        log.info("Invoice email placeholder for sale invoice: {}", salesOrder.getInvoiceNumber());
+//    }
+
     @Override
-    public void emailInvoice(SalesOrderHeader salesOrder) {
-        log.info("Invoice email placeholder for sale invoice: {}", salesOrder.getInvoiceNumber());
+    public void emailInvoice(String to, String subject, String body) {
+        try{
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(to);
+            mail.setSubject(subject);
+            mail.setText(body);
+           javaMailSender.send(mail);
+        }catch (Exception e){
+            log.error("Exception while send email "+e);
+        }
     }
 
     @Override
